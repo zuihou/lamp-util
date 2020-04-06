9bd03b9d924a9f7a07c1c270ccc56bfab22bcdf6
tangyh <244387066@qq.com>
2020-04-06 23:35:21 +0800

    完善文档和定时任务

---
+++ zuihou-config/src/main/java/com/github/zuihou/ConfigApplication.java
@@ -0,0 +1,13 @@
+package com.github.zuihou;
+
+/**
+ * 没什么用
+ *
+ * @author zuihou
+ * @date 2020年04月06日22:18:27
+ */
+public class ConfigApplication {
+    private String get() {
+        return "";
+    }
+}
