3d5b487fe68e4ce59138caf2fd85240d28e33ed4
zuihou <244387066@qq.com>
2022-05-19 22:42:06 +0800

    feat: 重构lamp-cloud项目
    1. 拆分boot模块为 boot 和 mvc模块
    2. 配置前缀中含有lamp的魔法字符串改成常量
    3. 将 lamp-annotation 中的model移动到core模块
    4. 删除RemoteData类和相关实现代码

---
+++ lamp-core/src/main/java/top/tangyh/basic/constant/Constants.java
@@ -0,0 +1,14 @@
+package top.tangyh.basic.constant;
+
+/**
+ * 全局基础常量
+ *
+ * @author tangyh
+ * @version v1.0
+ * @date 2022/4/12 11:05 PM
+ * @create [2022/4/12 11:05 PM ] [tangyh] [初始创建]
+ */
+public interface Constants {
+    /** 项目前缀 */
+    String PROJECT_PREFIX = "lamp";
+}
