17ecd952f26403871bc50b5b85981732f591a046
zuihou <244387066@qq.com>
2022-12-05 17:36:44 +0800

    feat: 适配新 版本 mybatis plus

--- lamp-databases/src/main/java/top/tangyh/basic/database/injector/method/UpdateAllById.java
+++ lamp-databases/src/main/java/top/tangyh/basic/database/injector/method/UpdateAllById.java
@@ -17,12 +17,12 @@ import java.util.function.Predicate;
 public class UpdateAllById extends AlwaysUpdateSomeColumnById {
 
     public UpdateAllById(Predicate<TableFieldInfo> predicate) {
-        super(predicate);
+        super("updateAllById", predicate);
     }
 
-    @Override
-    public String getMethod(SqlMethod sqlMethod) {
-        // 自定义 mapper 方法名
-        return "updateAllById";
-    }
+//    @Override
+//    public String getMethod(SqlMethod sqlMethod) {
+//        // 自定义 mapper 方法名
+//        return "updateAllById";
+//    }
 }
