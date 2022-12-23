366afe19d8f7a7220e324185b2dfc94ce63786b5
zuihou <244387066@qq.com>
2022-12-23 11:12:24 +0800

    feat: 重写 okhttp3.OkHttpClient 构造方式

--- lamp-cloud-starter/src/main/java/top/tangyh/basic/cloud/config/RestTemplateConfiguration.java
+++ lamp-cloud-starter/src/main/java/top/tangyh/basic/cloud/config/RestTemplateConfiguration.java
@@ -3,16 +3,13 @@ package top.tangyh.basic.cloud.config;
 
 import com.alibaba.cloud.sentinel.annotation.SentinelRestTemplate;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import top.tangyh.basic.cloud.http.InfoFeignLoggerFactory;
-import top.tangyh.basic.cloud.http.RestTemplateHeaderInterceptor;
 import feign.Logger;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import okhttp3.ConnectionPool;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.commons.httpclient.OkHttpClientConnectionPoolFactory;
-import org.springframework.cloud.commons.httpclient.OkHttpClientFactory;
-import org.springframework.cloud.openfeign.FeignClientProperties;
 import org.springframework.cloud.openfeign.FeignLoggerFactory;
 import org.springframework.cloud.openfeign.support.FeignHttpClientProperties;
 import org.springframework.context.annotation.Bean;
@@ -25,8 +22,17 @@ import org.springframework.web.client.RestTemplate;
 import top.tangyh.basic.cloud.http.InfoFeignLoggerFactory;
 import top.tangyh.basic.cloud.http.RestTemplateHeaderInterceptor;
 
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
 import java.time.Duration;
 import java.util.Collections;
 import java.util.List;
@@ -39,6 +45,7 @@ import java.util.concurrent.TimeUnit;
  */
 @ConditionalOnClass(okhttp3.OkHttpClient.class)
 @AllArgsConstructor
+@Slf4j
 public class RestTemplateConfiguration {
     private static final Charset UTF_8 = StandardCharsets.UTF_8;
     private final ObjectMapper objectMapper;
@@ -61,45 +68,117 @@ public class RestTemplateConfiguration {
         return Logger.Level.BASIC;
     }
 
-    /**
-     * 配置OkHttpClient
-     *
-     * @param httpClientFactory    httpClient 工厂
-     * @param connectionPool       链接池配置
-     * @param httpClientProperties httpClient配置
-     * @return OkHttpClient
-     */
+//    TODO java17
+//    /**
+//     * 配置OkHttpClient
+//     *
+//     * @param httpClientFactory    httpClient 工厂
+//     * @param connectionPool       链接池配置
+//     * @param httpClientProperties httpClient配置
+//     * @return OkHttpClient
+//     */
+//    @Bean
+//    @ConditionalOnMissingBean(okhttp3.OkHttpClient.class)
+//    public okhttp3.OkHttpClient okHttp3Client(
+//            OkHttpClientFactory httpClientFactory,
+//            okhttp3.ConnectionPool connectionPool,
+//            FeignClientProperties feignClientProperties,
+//            FeignHttpClientProperties httpClientProperties) {
+//        FeignClientProperties.FeignClientConfiguration defaultConfig = feignClientProperties.getConfig().get("default");
+//        return httpClientFactory.createBuilder(httpClientProperties.isDisableSslValidation())
+//                .followRedirects(httpClientProperties.isFollowRedirects())
+//                .writeTimeout(defaultConfig.getReadTimeout(), TimeUnit.MILLISECONDS)
+//                .readTimeout(defaultConfig.getReadTimeout(), TimeUnit.MILLISECONDS)
+//                .connectTimeout(httpClientProperties.getConnectionTimeout(), TimeUnit.MILLISECONDS)
+//                .connectionPool(connectionPool)
+//                .build();
+//    }
+//
+//    /**
+//     * okhttp3 链接池配置
+//     *
+//     * @param connectionPoolFactory 链接池配置
+//     * @param hcp                   httpClient配置
+//     * @return okhttp3.ConnectionPool
+//     */
+//    @Bean
+//    @ConditionalOnMissingBean(okhttp3.ConnectionPool.class)
+//    public okhttp3.ConnectionPool okHttp3ConnectionPool(FeignHttpClientProperties hcp,
+//                                                        OkHttpClientConnectionPoolFactory connectionPoolFactory) {
+//        return connectionPoolFactory.create(hcp.getMaxConnections(), hcp.getTimeToLive(), hcp.getTimeToLiveUnit());
+//    }
+
     @Bean
-    @ConditionalOnMissingBean(okhttp3.OkHttpClient.class)
-    public okhttp3.OkHttpClient okHttp3Client(
-            OkHttpClientFactory httpClientFactory,
-            okhttp3.ConnectionPool connectionPool,
-            FeignClientProperties feignClientProperties,
-            FeignHttpClientProperties httpClientProperties) {
-        FeignClientProperties.FeignClientConfiguration defaultConfig = feignClientProperties.getConfig().get("default");
-        return httpClientFactory.createBuilder(httpClientProperties.isDisableSslValidation())
-                .followRedirects(httpClientProperties.isFollowRedirects())
-                .writeTimeout(defaultConfig.getReadTimeout(), TimeUnit.MILLISECONDS)
-                .readTimeout(defaultConfig.getReadTimeout(), TimeUnit.MILLISECONDS)
-                .connectTimeout(httpClientProperties.getConnectionTimeout(), TimeUnit.MILLISECONDS)
-                .connectionPool(connectionPool)
-                .build();
+    @ConditionalOnMissingBean
+    public okhttp3.OkHttpClient.Builder okHttpClientBuilder() {
+        return new okhttp3.OkHttpClient.Builder();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(ConnectionPool.class)
+    public ConnectionPool httpClientConnectionPool(FeignHttpClientProperties httpClientProperties) {
+        int maxTotalConnections = httpClientProperties.getMaxConnections();
+        long timeToLive = httpClientProperties.getTimeToLive();
+        TimeUnit ttlUnit = httpClientProperties.getTimeToLiveUnit();
+        return new ConnectionPool(maxTotalConnections, timeToLive, ttlUnit);
+    }
+
+    @Bean
+    public okhttp3.OkHttpClient okHttpClient(okhttp3.OkHttpClient.Builder builder, ConnectionPool connectionPool, FeignHttpClientProperties httpClientProperties) {
+        boolean followRedirects = httpClientProperties.isFollowRedirects();
+        int connectTimeout = httpClientProperties.getConnectionTimeout();
+        boolean disableSslValidation = httpClientProperties.isDisableSslValidation();
+        Duration readTimeout = httpClientProperties.getOkHttp().getReadTimeout();
+        if (disableSslValidation) {
+            disableSsl(builder);
+        }
+        return builder.connectTimeout(connectTimeout, TimeUnit.MILLISECONDS)
+                .followRedirects(followRedirects).readTimeout(readTimeout).connectionPool(connectionPool).build();
+    }
+
+    private void disableSsl(okhttp3.OkHttpClient.Builder builder) {
+        try {
+            X509TrustManager disabledTrustManager = new RestTemplateConfiguration.DisableValidationTrustManager();
+            TrustManager[] trustManagers = new TrustManager[1];
+            trustManagers[0] = disabledTrustManager;
+            SSLContext sslContext = SSLContext.getInstance("SSL");
+            sslContext.init(null, trustManagers, new java.security.SecureRandom());
+            SSLSocketFactory disabledSSLSocketFactory = sslContext.getSocketFactory();
+            builder.sslSocketFactory(disabledSSLSocketFactory, disabledTrustManager);
+            builder.hostnameVerifier(new RestTemplateConfiguration.TrustAllHostnames());
+        } catch (NoSuchAlgorithmException | KeyManagementException e) {
+            log.warn("Error setting SSLSocketFactory in OKHttpClient", e);
+        }
     }
 
     /**
-     * okhttp3 链接池配置
-     *
-     * @param connectionPoolFactory 链接池配置
-     * @param hcp                   httpClient配置
-     * @return okhttp3.ConnectionPool
+     * A {@link X509TrustManager} that does not validate SSL certificates.
      */
-    @Bean
-    @ConditionalOnMissingBean(okhttp3.ConnectionPool.class)
-    public okhttp3.ConnectionPool okHttp3ConnectionPool(FeignHttpClientProperties hcp,
-                                                        OkHttpClientConnectionPoolFactory connectionPoolFactory) {
-        return connectionPoolFactory.create(hcp.getMaxConnections(), hcp.getTimeToLive(), hcp.getTimeToLiveUnit());
+    class DisableValidationTrustManager implements X509TrustManager {
+
+        @Override
+        public void checkClientTrusted(X509Certificate[] x509Certificates, String s) {
+        }
+
+        @Override
+        public void checkServerTrusted(X509Certificate[] x509Certificates, String s) {
+        }
+
+        @Override
+        public X509Certificate[] getAcceptedIssuers() {
+            return new X509Certificate[0];
+        }
+
     }
 
+    class TrustAllHostnames implements HostnameVerifier {
+
+        @Override
+        public boolean verify(String s, SSLSession sslSession) {
+            return true;
+        }
+
+    }
 
     /**
      * 解决 RestTemplate 传递Request header
