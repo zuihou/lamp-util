648abffe1cd527a78846c21a3c2b81452566b3bf
zuihou <244387066@qq.com>
2022-12-25 21:39:42 +0800

    build: 升级 knife4j 4.0.0
    - knife4j.version>4.0.0
    - springfox.version>3.0.0
    - knife4j-swagger-models-v3.version>2.2.7
    - swagger在线文档由springfox 替换为 springdoc

--- lamp-swagger2-starter/src/main/java/top/tangyh/basic/swagger2/Swagger2Configuration.java
+++ lamp-swagger2-starter/src/main/java/top/tangyh/basic/swagger2/Swagger2Configuration.java
@@ -1,20 +1,8 @@
 package top.tangyh.basic.swagger2;
 
-import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Import;
-import org.springframework.util.ReflectionUtils;
-import org.springframework.web.servlet.mvc.method.RequestMappingInfoHandlerMapping;
-import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
-import springfox.documentation.spring.web.plugins.WebMvcRequestHandlerProvider;
-import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
-
-import java.lang.reflect.Field;
-import java.util.List;
-import java.util.stream.Collectors;
 
 import static top.tangyh.basic.swagger2.Swagger2Configuration.TRUE;
 
@@ -27,9 +15,7 @@ import static top.tangyh.basic.swagger2.Swagger2Configuration.TRUE;
  * @author zuihou
  * @date 2018/11/18 9:20
  */
-@EnableSwagger2WebMvc
 @ConditionalOnProperty(prefix = "knife4j", name = "enable", havingValue = TRUE, matchIfMissing = true)
-@Import(BeanValidatorPluginsConfiguration.class)
 public class Swagger2Configuration {
 
     public static final String TRUE = "true";
@@ -40,43 +26,4 @@ public class Swagger2Configuration {
         return new SwaggerWebMvcConfigurer();
     }
 
-
-    /**
-     * 升级springboot2.6.6后临时处理，防止swagger报错
-     *
-     * @author tangyh
-     * @date 2022/4/9 7:18 PM
-     * @create [2022/4/9 7:18 PM ] [tangyh] [初始创建]
-     */
-    @Bean
-    public BeanPostProcessor springfoxHandlerProviderBeanPostProcessor() {
-        return new BeanPostProcessor() {
-            @Override
-            public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
-                if (bean instanceof WebMvcRequestHandlerProvider) {
-                    customizeSpringfoxHandlerMappings(getHandlerMappings(bean));
-                }
-                return bean;
-            }
-
-            private <T extends RequestMappingInfoHandlerMapping> void customizeSpringfoxHandlerMappings(List<T> mappings) {
-                List<T> copy = mappings.stream()
-                        .filter(mapping -> mapping.getPatternParser() == null)
-                        .collect(Collectors.toList());
-                mappings.clear();
-                mappings.addAll(copy);
-            }
-
-            @SuppressWarnings("unchecked")
-            private List<RequestMappingInfoHandlerMapping> getHandlerMappings(Object bean) {
-                try {
-                    Field field = ReflectionUtils.findField(bean.getClass(), "handlerMappings");
-                    field.setAccessible(true);
-                    return (List<RequestMappingInfoHandlerMapping>) field.get(bean);
-                } catch (IllegalArgumentException | IllegalAccessException e) {
-                    throw new IllegalStateException(e);
-                }
-            }
-        };
-    }
 }
