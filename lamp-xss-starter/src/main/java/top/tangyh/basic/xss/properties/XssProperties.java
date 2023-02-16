f4b143ec2c080e02af98672ecaf29259bf83b54f
zuihou <244387066@qq.com>
2023-02-16 10:10:49 +0800

    feat: 统一变量名

--- lamp-xss-starter/src/main/java/top/tangyh/basic/xss/properties/XssProperties.java
+++ lamp-xss-starter/src/main/java/top/tangyh/basic/xss/properties/XssProperties.java
@@ -3,6 +3,7 @@ package top.tangyh.basic.xss.properties;
 import cn.hutool.core.collection.CollUtil;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import top.tangyh.basic.constant.Constants;
 
 import java.util.List;
 
@@ -15,7 +16,7 @@ import java.util.List;
 @Data
 @ConfigurationProperties(prefix = XssProperties.PREFIX)
 public class XssProperties {
-    public static final String PREFIX = "lamp.xss";
+    public static final String PREFIX = Constants.PROJECT_PREFIX + ".xss";
     private Boolean enabled = true;
     /**
      * 是否启用 RequestBody 注解标记的参数 反序列化时过滤XSS
