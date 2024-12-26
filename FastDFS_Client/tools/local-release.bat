d203cb218c9aeae5456694a41888c3037918ef6a
zuihou <244387066@qq.com>
2024-12-26 10:30:53 +0800

    feat: 统一jar groupId

--- FastDFS_Client/tools/local-release.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo [INFO] build and install modules.
-cd ..\
-call mvn clean deploy -P local-release
-pause
--- FastDFS_Client/tools/pakcage-skipTest.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo [INFO] build and install modules.
-cd ..\
-call mvn clean package -Dmaven.test.skip=true 
-pause
